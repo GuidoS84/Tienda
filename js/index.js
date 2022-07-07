fa1d01a7fc025a9485aab4404f1c49c280f1d19b
Guido Serbali <guidoserbali@gmail.com>
2022-07-07 14:10:43 -0300

    agregar productos al carrito + localStorage

--- js/index.js
+++ js/index.js
@@ -28,11 +28,25 @@ for (const producto of productos)
 
 console.log(productos);
 
-//CARRITO
-const carrito =[];
+
 
 //DOM
-let cards = document.getElementById('cardProd');
+const cards = document.getElementById('cardProd');
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+const botonVaciar = document.getElementById('vaciar-carrito')
+const contadorCarrito = document.getElementById('contadorCarrito')
+const precioTotal = document.getElementById('precioTotal')
+
+// CARDS PRODUCTOS
+
+let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+window.addEventListener("load", () => {
+  if (carrito.length > 0) {
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+    actualizarCarrito();
+  }
+})
 
 
 productos.forEach(producto => {
@@ -43,16 +57,71 @@ productos.forEach(producto => {
                     <div class="card-body">
                       <h5 class="card-title">${producto.nombre}</h5>
                       <p class="card-text">$${producto.precio}</p>
-                      <button id="comprar" onclick="Comprar(${producto.id})"  class="btn btn-primary">Comprar</button>
+                      <button id= "agregar${producto.id}" class="btn btn-primary boton-agregar">Comprar</button>
                     </div>  
   </div>
 `
-  cards.append(card);
-})
+  cards.appendChild(card);
+  
+  const boton = document.getElementById(`agregar${producto.id}`)
 
-//FUNCIONES
-const Comprar = (x) =>{
-  carrito.push(productos[x]);
-  console.log(carrito);
+  boton.addEventListener('click', ()=> {
+    console.log(boton);
+    agregarAlCarrito(producto.id)
+    console.log(carrito);
+
+  })
+
+})
+// AGREGA LOS PRODUCTOS AL CARRITO Y PEPETIR CANTIDAD
+const agregarAlCarrito = (prodId) => {
+  const existe = carrito.some(prod => prod.id == prodId)
+    
+    if(existe) {
+    const prod = carrito.map (prod => {
+      if (prod.id === prodId) {
+          prod.cantidad++
+      }
+    })
+  }else {
+    const item = productos.find((prod) => prod.id === prodId)
+    carrito.push(item);
+    console.log(item);
+    
+  }  
+actualizarCarrito();
 }
 
+// AGREGAMOS UN PROD AL CARRITO
+
+const actualizarCarrito = () => {
+  //borra el nodo y despues recorre el array
+  contenedorCarrito.innerHTML = "";  
+    carrito.forEach((prod) => {
+    const div = document.createElement('div')
+   
+    div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad${prod.cantidad}"></span></p>
+        <button id="eliminarDelCarrito${prod.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+        contenedorCarrito.appendChild(div);
+
+        //setItem
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+        
+  })
+  
+  contadorCarrito.innerText = carrito.length
+
+  precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0)
+  
+}
+
+
+
+botonVaciar.addEventListener('click', () => {
+  carrito.length = 0
+  actualizarCarrito()
+})
